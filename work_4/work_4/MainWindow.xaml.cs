b6875e1ee71999f703d414ae98a87dff37ed66d0
Fifteen417 <vannessgoh888@gmail.com>
2023-03-27 14:55:03 +0800

    3/27 14:25
    add calculation function

--- work_4/work_4/MainWindow.xaml.cs
+++ work_4/work_4/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace work_4
     /// </summary>
     public partial class MainWindow : Window
     {
+        float firstNumber, secondNumber;
+        int operators = -1; //0:加, 1:減, 2:乘, 3:除, -1:重新設定
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,50 +54,108 @@ namespace work_4
         private void btn_3_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "3";
         }
 
         private void btn_4_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "4";
         }
 
         private void btn_5_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "5";
         }
 
         private void btn_6_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "6";
         }
 
         private void btn_7_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "7";
         }
 
         private void btn_8_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "8";
         }
 
         private void btn_9_Click(object sender, RoutedEventArgs e)
         {
             if (txt_cal.Text == "0")
-                txt_cal.Text = ""; //只有一行可以省略大括號
+                txt_cal.Text = "";
             txt_cal.Text += "9";
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle (txt_cal.Text); // 把文字框轉換成浮點數，存入第一個數字的全域變數
+            txt_cal.Text = "0"; // 重置文字框
+            operators = 0; // 選擇加號 "0 = 「+」"
+        }
+
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txt_cal.Text);
+            txt_cal.Text = "0";
+            operators = 1; // 選擇減號 "1 = 「-」"
+        }
+
+        private void btn_times_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txt_cal.Text);
+            txt_cal.Text = "0";
+            operators = 2; // 選擇乘號 "2 = 「*」"
+        }
+
+        private void btn_div_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txt_cal.Text);
+            txt_cal.Text = "0";
+            operators = 3; // 選擇除號 "3 = 「/」"
+        }
+
+        private void btn_equal_Click(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f; // 宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txt_cal.Text); // 把文字框轉換成浮點數，存入第二個數字的全域變數
+
+            // 依照選擇進行運算 "加減乘除"
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txt_cal.Text = string.Format("{0:0.#########}", finalResults); // 在文字框中顯示最後的結果
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
 
         private void btn_AC_Click(object sender, RoutedEventArgs e)
